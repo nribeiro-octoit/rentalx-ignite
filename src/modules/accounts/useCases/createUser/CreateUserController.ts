1b02f03ef340655706956011c30c4043ac20c676
Nadson Dias Ribeiro <nadson.dr11@gmail.com>
2021-04-13 00:46:42 -0300

    Alterar tabela de usuário

--- src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -5,13 +5,7 @@ import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const {
-            name,
-            email,
-            password,
-            username,
-            driver_license,
-        } = request.body;
+        const { name, email, password, driver_license } = request.body;
 
         const createUserUseCase = container.resolve(CreateUserUseCase);
 
@@ -19,7 +13,6 @@ class CreateUserController {
             name,
             email,
             password,
-            username,
             driver_license,
         });
 
