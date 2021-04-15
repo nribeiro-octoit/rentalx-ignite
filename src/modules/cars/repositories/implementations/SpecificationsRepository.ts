aab50a5ac4359f525ac55574f99c3185090b5ca9
Nadson Dias Ribeiro <nadson.dr11@gmail.com>
2021-04-15 12:43:34 -0300

    Imports da aplicação

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,9 +1,10 @@
 import { getRepository, Repository } from "typeorm";
 
-import { Specification } from "../../entities/Specification";
+import { Specification } from "@modules/cars/entities/Specification";
+
 import {
-    ICreateSpecificationDTO,
     ISpecificationsRepository,
+    ICreateSpecificationDTO,
 } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
